5eb6405445e4fd7d20a4514025cfda5865f9d4d4
Zasilus <asg101@case.edu>
2018-10-20 10:47:38 -0400

    created a working scraper returns information as a JSON
    Signed-off-by: Zasilus <asg101@case.edu>

--- RMPLookup.py
+++ RMPLookup.py
@@ -3,7 +3,7 @@
 import requests
 from bs4 import BeautifulSoup
 from ProfessorInfo import ProfessorInfo
-
+import json
 
 class RMPLookup:
 
@@ -22,17 +22,48 @@ class RMPLookup:
             page = requests.get(requestsurl)
             soup = BeautifulSoup(page.content, 'html.parser')
             # soup = soup.encode("utf-8")
-            new_soup = soup.find("li", {"class": "listing PROFESSOR"})
-            if (new_soup == None):
+            find_professor = soup.find("li", {"class": "listing PROFESSOR"})
+            if (find_professor == None):
                 continue
             else:
-                a_ref = new_soup.find("a")
-                print(professor_name)
+                a_ref = find_professor.find("a")
                 for char in a_ref['href']:
                     baseurl += str(char)
-                print(baseurl)
                 url_list.append(baseurl)
         return url_list
+
+    def class_teaching_professor_list(self, class_code, url_list):
+        list_of_dict = []
+        for professor_url in url_list:
+            page = requests.get(professor_url)
+            soup = BeautifulSoup(page.content, 'html.parser')
+            find_class = soup.find_all("span", {"class": "name"})
+            is_teaching_class = False
+            for span in find_class:
+                class_name = span.find("span", {"class": "response"}).text
+                if class_name == class_code:
+                    is_teaching_class = True
+                    break
+            if is_teaching_class:
+                prof_dict = self.scrap_difficulty_rating(soup)
+                list_of_dict.append(prof_dict)
+        json_output = json.dumps(list_of_dict)
+        return json_output
+
+
+    def scrap_difficulty_rating(self,soup):
+        professor_name_span = soup.find("span", {"class": "pfname"}).text
+        professor_name_span = professor_name_span.strip(' \t\n\r')
+        professor_last_name_span = soup.find("span", {"class": "plname"}).text
+        professor_last_name_span = professor_last_name_span.strip(' \t\n\r')
+        prof_name = professor_name_span + " " + professor_last_name_span
+        prof_scrap = soup.find_all("div", {"class": "grade"})
+        prof_rating = prof_scrap[0].text.strip(' \t\n\r')
+        prof_difficulty = prof_scrap[2].text.strip(' \t\n\r')
+        prof_dict = { "name": prof_name, "rating": prof_rating, "difficulty": prof_difficulty}
+        return prof_dict
+
+
 
     def getdepartmentlist(self, classcode, professorlist):
         departmentname = self.classcodetodepartment(classcode)
@@ -76,8 +107,9 @@ def main():
     prompt = ProfessorInfo()
     genericlist = prompt.make_prof_dept_list()
 
-    print(lookup.getdepartmentlist("CHEM", genericlist));
-    print(lookup.lookupprofessor("CHEM", genericlist));
+    lookup.getdepartmentlist("CHEM", genericlist);
+    url_list = lookup.lookupprofessor("CHEM", genericlist);
+    lookup.class_teaching_professor_list("CHEM223", url_list);
     #print(lookup.getdepartmentlist("CHEM", genericlist));
     #print(lookup.getdepartmentlist("DANC", genericlist));
 
